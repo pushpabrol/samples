c8920a76aca29bc3319cfd9743cf43edcf515d75
pushpabrol <pushp.abrol@gmail.com>
2023-02-17 13:48:17 -0500

    Added OIDC Sample for CHFS

---
+++ CHFSOIDCSample/Startup.cs
@@ -0,0 +1,87 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using SampleMvcApp.Support;
+using Auth0.AspNetCore.Authentication;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using System;
+
+namespace SampleMvcApp
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.ConfigureSameSiteNoneCookies();
+
+            services.AddAuth0WebAppAuthentication(options => {
+                options.Domain = Configuration["Auth0:Domain"];
+                options.ClientId = Configuration["Auth0:ClientId"];
+                options.OpenIdConnectEvents = new Microsoft.AspNetCore.Authentication.OpenIdConnect.OpenIdConnectEvents()
+                {
+
+                    OnRedirectToIdentityProvider = context => {
+                        context.ProtocolMessage.SetParameter("display", context.Request.BaseUrl());
+                        context.ProtocolMessage.SetParameter("connection", Configuration["Auth0:Connection"]);
+                        return Task.CompletedTask;
+                    }
+
+                }; 
+
+            });
+
+            services.AddControllersWithViews();
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                app.UseHsts();
+            }
+
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseRouting();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapDefaultControllerRoute();
+            });
+        }
+    }
+
+    public static class HttpRequestExtensions
+    {
+        public static string BaseUrl(this HttpRequest req)
+        {
+            if (req == null) return null;
+            var uriBuilder = new UriBuilder(req.Scheme, req.Host.Host, req.Host.Port ?? -1);
+            if (uriBuilder.Uri.IsDefaultPort)
+            {
+                uriBuilder.Port = -1;
+            }
+
+            return uriBuilder.Uri.AbsoluteUri;
+        }
+    }
+}
